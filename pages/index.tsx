c1ea4e948b0f3068b8a3de88e0d3aa72400ee83e
Dave Perrett <dave@recurser.com>
2021-10-05 21:30:21 +0900

    Add grid layout and update favicons.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,17 @@
-import { Paragraph, Textarea } from 'evergreen-ui'
+import { Pane, Paragraph, Textarea } from 'evergreen-ui'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styledComponents from 'styled-components'
-
-const StyledTest = styledComponents.div`
-  border: 1px solid #efefef;
-`
 
 const Home: NextPage = () => {
   return (
-    <StyledTest>
+    <Pane>
       <Head>
         <title>Dev Toolbar</title>
-        <link href="/favicon.ico" rel="icon" />
       </Head>
 
       <Paragraph>Paste here</Paragraph>
       <Textarea />
-    </StyledTest>
+    </Pane>
   )
 }
 
--- src/components/layouts/ApplicationLayout.tsx
+++ src/components/layouts/ApplicationLayout.tsx
@@ -1,24 +1,67 @@
-import { Image, majorScale, Pane, Paragraph } from 'evergreen-ui'
+import { Image, majorScale, Pane, Tab, Tablist } from 'evergreen-ui'
 
 import Logo from '@images/logo.svg'
-import { theme } from '@services/Theme'
+import { useBreakpoints } from '@services/Responsive'
 
 export const ApplicationLayout = ({ children }) => {
+  const breakpoints = useBreakpoints()
+
+  const gap = `${majorScale(2)}px ${majorScale(4)}px`
+  const padTop = majorScale(3)
+  const rows = `${majorScale(4)}px 1fr`
+  let columns
+  const areas =
+    "'left-pane menu-pane right-pane' 'left-pane content-pane right-pane'"
+
+  if (breakpoints.isBigscreen) {
+    columns = 'auto 1000px auto'
+  } else if (breakpoints.isDesktop) {
+    columns = '1fr 4fr 1fr'
+  } else {
+    columns = '0fr 1fr 0fr'
+  }
+
   return (
-    <Pane>
-      <Pane>
-        <Image
-          alt="Dev Toolbar Logo"
-          borderRadius={theme.radii[1]}
-          height={majorScale(5)}
-          src={`${Logo.src}`}
-          width={majorScale(5)}
-        />
+    <>
+      <Pane
+        display="grid"
+        gridGap={gap}
+        gridTemplateAreas={areas}
+        gridTemplateColumns={columns}
+        gridTemplateRows={rows}
+        justifyContent="center"
+        paddingTop={padTop}
+      >
+        <Pane
+          alignItems="center"
+          display="flex"
+          flexDirection="row"
+          gridArea="menu-pane"
+          justifyContent="space-between"
+        >
+          <Image
+            alt="Dev Toolbar Logo"
+            height={majorScale(5)}
+            src={`${Logo.src}`}
+            width={majorScale(5)}
+          />
+          <Pane>
+            <Tablist>
+              <Tab isSelected={true} onSelect={() => undefined}>
+                Home
+              </Tab>
+              <Tab isSelected={false} onSelect={() => undefined}>
+                About
+              </Tab>
+              <Tab isSelected={false} onSelect={() => undefined}>
+                Github
+              </Tab>
+            </Tablist>
+          </Pane>
+        </Pane>
+
+        <Pane gridArea="content-pane">{children}</Pane>
       </Pane>
-
-      <Pane>{children}</Pane>
-
-      <Paragraph>Footer</Paragraph>
-    </Pane>
+    </>
   )
 }
