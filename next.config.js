bdb649c402d70345ebe15e5de4eb4f979c45d4c7
Dave Perrett <dave@recurser.com>
2021-10-07 01:12:28 +0900

    Put headers outside of next-translate to get CSP working on the root path.

--- next.config.js
+++ next.config.js
@@ -3,68 +3,72 @@
 const nextTranslate = require('next-translate')
 
 const dev = process.env.NODE_ENV !== 'production'
+const scriptSrc = dev ? "'self' 'unsafe-inline' 'unsafe-eval'" : "'self'"
+const cspHeaders = [
+  {
+    key: 'Access-Control-Allow-Origin',
+    value:
+      'https://string.test https://staging.string.is https://www.string.is',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value:
+      `base-uri 'none'; ` +
+      `connect-src 'self' https://vitals.vercel-insights.com; ` +
+      `default-src 'none'; ` +
+      `font-src 'self'; ` +
+      `form-action 'self'; ` +
+      `frame-ancestors 'self'; ` +
+      `img-src 'self' data: ; ` +
+      `manifest-src 'self'; ` +
+      `prefetch-src 'self'; ` +
+      `script-src ${scriptSrc}; ` +
+      `script-src-elem ${scriptSrc}; ` +
+      `style-src 'self' 'unsafe-inline';`,
+  },
+  {
+    key: 'Permissions-Policy',
+    value:
+      'autoplay=(), camera=(), fullscreen=(self), geolocation=(), microphone=(), payment=(), usb=()',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=15768000; includeSubDomains',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'same-origin',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Download-Options',
+    value: 'noopen',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Permitted-Cross-Domain-Policies',
+    value: 'none',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+]
 
 const headers = async () => {
-  const scriptSrc = dev ? "'self' 'unsafe-inline' 'unsafe-eval'" : "'self'"
   return [
     {
-      basePath: undefined,
-      headers: [
-        {
-          key: 'Access-Control-Allow-Origin',
-          value:
-            'https://string.test https://staging.string.is https://www.string.is',
-        },
-        {
-          key: 'Content-Security-Policy',
-          value:
-            `base-uri 'none'; ` +
-            `connect-src 'self' https://vitals.vercel-insights.com; ` +
-            `default-src 'none'; ` +
-            `font-src 'self'; ` +
-            `form-action 'self'; ` +
-            `frame-ancestors 'self'; ` +
-            `img-src 'self' data: ; ` +
-            `manifest-src 'self'; ` +
-            `prefetch-src 'self'; ` +
-            `script-src ${scriptSrc}; ` +
-            `script-src-elem ${scriptSrc}; ` +
-            `style-src 'self' 'unsafe-inline';`,
-        },
-        {
-          key: 'Permissions-Policy',
-          value:
-            'autoplay=(), camera=(), fullscreen=(self), geolocation=(), microphone=(), payment=(), usb=()',
-        },
-        {
-          key: 'Strict-Transport-Security',
-          value: 'max-age=15768000; includeSubDomains',
-        },
-        {
-          key: 'Referrer-Policy',
-          value: 'same-origin',
-        },
-        {
-          key: 'X-Content-Type-Options',
-          value: 'nosniff',
-        },
-        {
-          key: 'X-Download-Options',
-          value: 'noopen',
-        },
-        {
-          key: 'X-Frame-Options',
-          value: 'DENY',
-        },
-        {
-          key: 'X-Permitted-Cross-Domain-Policies',
-          value: 'none',
-        },
-        {
-          key: 'X-XSS-Protection',
-          value: '1; mode=block',
-        },
-      ],
+      headers: cspHeaders,
+      source: '/',
+    },
+    {
+      headers: cspHeaders,
       source: '/(.*?)',
     },
     {
@@ -79,11 +83,14 @@ const headers = async () => {
   ]
 }
 
-module.exports = nextTranslate({
-  headers,
+const cfg = nextTranslate({
   reactStrictMode: true,
-  trailingSlash: true, // CSP headers aren't returned on the root page if this is false.
   webpack: (config, { isServer, webpack }) => {
     return config
   },
 })
+
+module.exports = {
+  ...cfg,
+  headers,
+}
