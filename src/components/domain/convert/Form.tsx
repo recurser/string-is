1a19134ded31c1e7c1b0fc106195b72f2b02be7f
Dave Perrett <dave@recurser.com>
2021-10-12 22:21:13 +0900

    Auto-focus on the input field, and tweak the label.

--- src/components/domain/convert/Form.tsx
+++ src/components/domain/convert/Form.tsx
@@ -1,16 +1,21 @@
-import { Paragraph, Textarea } from 'evergreen-ui'
+import { Pane, Paragraph, Textarea } from 'evergreen-ui'
 import { debounce } from 'lodash'
 import useTranslation from 'next-translate/useTranslation'
-import { ChangeEvent, useEffect, useMemo, useState } from 'react'
+import { ChangeEvent, useEffect, useMemo, createRef, useState } from 'react'
 
 import { useInputContext } from '@contexts/InputContext'
 
 export const Form = () => {
   const { t } = useTranslation('domain-convert-form')
-
   const { setInputString } = useInputContext()
-
   const [input, setInput] = useState('')
+  const inputRef = createRef<HTMLTextAreaElement>()
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [inputRef])
 
   const doDebounce = useMemo(
     () => debounce((data: string) => setInputString(data), 300),
@@ -23,9 +28,17 @@ export const Form = () => {
     setInput(event.target.value)
 
   return (
-    <>
+    <Pane>
       <Paragraph role="label">{t('label')}</Paragraph>
-      <Textarea onChange={onChange} value={input} />
-    </>
+      <Textarea
+        autoFocus={
+          true
+        } /* This doesn't seem to do anything, but might help in some browsers? */
+        onChange={onChange}
+        placeholder={t('placeholder')}
+        ref={inputRef}
+        value={input}
+      />
+    </Pane>
   )
 }
