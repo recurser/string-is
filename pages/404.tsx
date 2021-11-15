e7d0e5efa581d3c8b7145b562192b9de4183b653
Dave Perrett <dave@recurser.com>
2021-11-15 23:37:54 +0900

    Track 404 pages.

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,9 +2,18 @@ import type { ReactElement } from 'react'
 
 import { Application } from '@components/layout'
 import { NotFound as SubNotFound } from '@pages/errors/NotFound'
+import { useAnalytics } from '@services/Analytics'
 
 // eslint-disable-next-line import/no-default-export
 export default function NotFound() {
+  const analytics = useAnalytics()
+
+  analytics('404', {
+    props: {
+      path: document.location.pathname,
+    },
+  })
+
   return <SubNotFound />
 }
 
