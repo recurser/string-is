edce2d7c4b5d62aa960fb8069a4c16e7e8a03bf4
Dave Perrett <dave@recurser.com>
2021-11-15 22:36:38 +0900

    Remove support for data-in-url that may get exposed in analytics.

--- i18n.js
+++ i18n.js
@@ -1,27 +1,24 @@
 const common = ['common', 'layout-footer', 'layout-header']
 
-const convert = [
-  ...common,
-  'domain-convert-converterSelector',
-  'domain-convert-inputForm',
-  'domain-convert-outputForm',
-  'domain-convert-outputs-csvOutput',
-  'domain-convert-outputs-htmlOutput',
-  'domain-convert-outputs-jsonOutput',
-  'domain-convert-outputs-shaOutput',
-  'domain-convert-outputs-yamlOutput',
-  'lib-converters-commands',
-  'lib-converters-results',
-  'pages-convert',
-]
-
 module.exports = {
   defaultLocale: 'en',
   locales: ['en'],
   pages: {
-    '/': convert,
+    '/': [
+      ...common,
+      'domain-convert-converterSelector',
+      'domain-convert-inputForm',
+      'domain-convert-outputForm',
+      'domain-convert-outputs-csvOutput',
+      'domain-convert-outputs-htmlOutput',
+      'domain-convert-outputs-jsonOutput',
+      'domain-convert-outputs-shaOutput',
+      'domain-convert-outputs-yamlOutput',
+      'lib-converters-commands',
+      'lib-converters-results',
+      'pages-convert',
+    ],
     '/404': [...common, 'pages-errors-notFound'],
-    '/[input]': convert,
     '/about': [...common, 'pages-about'],
     '/privacy': [...common, 'pages-privacy'],
     '/security': [...common, 'pages-security'],
--- pages/[input].tsx
+++
@@ -1,6 +0,0 @@
-import Home from './index'
-
-// This is a separate route to allow the input string to be
-// provided as a URL parameter.
-
-export default Home // eslint-disable-line import/no-default-export
