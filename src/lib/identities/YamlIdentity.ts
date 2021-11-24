9e9cef2e504e80c8d0d0ed3b9181d9aef59a0f6d
Dave Perrett <dave@recurser.com>
2021-11-24 22:22:53 +0900

    Require YAML strings to have at least two lines.

--- src/lib/identities/YamlIdentity.ts
+++ src/lib/identities/YamlIdentity.ts
@@ -4,6 +4,12 @@ import { input } from '@lib/inputs/YamlInput'
 export const id = 'yaml'
 
 export const confidence = (data: string) => {
+  // We can assume that YAML will have at least 2 lines. Any number
+  // is actually valid YAML, so eg. timestamps will trigger it.
+  if (data.split(/\r\n|\r|\n/).length < 2) {
+    return 0
+  }
+
   const obj = input(data)
   if (!obj) {
     return 0
