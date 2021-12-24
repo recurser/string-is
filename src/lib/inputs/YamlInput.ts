b4f4f2b42afd6f33ee64bef1d012eb9a70439caf
Dave Perrett <dave@recurser.com>
2021-12-24 16:57:46 +0900

    Ignore strings that failed to load as YAML.

--- src/lib/inputs/YamlInput.ts
+++ src/lib/inputs/YamlInput.ts
@@ -3,13 +3,19 @@ import { isEmpty } from 'lodash'
 
 import { Obj } from '@lib/types'
 
-export const input = (input: string): string | Obj | undefined => {
+export const input = (input: string): Obj | undefined => {
   if (isEmpty(input)) {
     return undefined
   }
 
   try {
-    return load(input) as string | Obj
+    const result = load(input)
+
+    if (typeof result === 'string') {
+      return undefined
+    }
+
+    return result as Obj
   } catch (err) {
     return undefined
   }
