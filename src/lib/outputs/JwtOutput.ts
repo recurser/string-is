8e771d38a46e72e4913bef4c8e93ac8a9a76abb6
Dave Perrett <dave@recurser.com>
2021-12-30 09:50:39 +0900

    [#98] Improve error handling for JWT input.

--- src/lib/outputs/JwtOutput.ts
+++ src/lib/outputs/JwtOutput.ts
@@ -1,12 +1,18 @@
 import type { Jwt } from 'jsonwebtoken'
 
+import { input as jwtInput } from '@lib/inputs/JwtInput'
 import { output as jsonOutput } from '@lib/outputs/JsonOutput'
 import { ConverterOptions, Obj } from '@lib/types'
 
 export const id = 'jwt'
 
-export const header = (jwt: Jwt, _options: ConverterOptions = {}): string => {
-  return jsonOutput(jwt.header as unknown as Obj)
+export const header = (data: string, _options: ConverterOptions = {}): string => {
+  try {
+    const jwt = jwtInput(data)
+    return jsonOutput(jwt.header as unknown as Obj)
+  } catch (err) {
+    return ''
+  }
 }
 
 export const output = (jwt: Jwt, _options: ConverterOptions = {}): string => {
