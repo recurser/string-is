8c6e8033564533893a183fd7f7de083bd7142b57
Dave Perrett <dave@recurser.com>
2021-12-30 16:58:08 +0900

    [#98] Improve error handling for Timestamp input.

--- src/lib/outputs/DatetimeOutput.ts
+++ src/lib/outputs/DatetimeOutput.ts
@@ -19,6 +19,10 @@ export const id = 'datetime'
 const parse = (input: string, timezone: string): Dayjs => {
   const multiplier = input.length === 10 ? 1000 : 1
   const datetime = new Date(parseInt(input, 10) * multiplier)
+  if (isNaN(datetime.getTime())) {
+    throw new Error('The input is not a valid timestamp')
+  }
+
   return dayjs(datetime).tz(timezone)
 }
 
@@ -32,11 +36,19 @@ export const relativeOutput = (
   input: string,
   options: ConverterOptions,
 ): string => {
-  const date = parse(input, options.timezone as string)
+  try {
+    const date = parse(input, options.timezone as string)
 
-  return dayjs().to(date)
+    return dayjs().to(date)
+  } catch (err) {
+    return ''
+  }
 }
 
 export const utcOutput = (input: string, options: ConverterOptions): string => {
-  return parse(input, 'UTC').format(options.format as string)
+  try {
+    return parse(input, 'UTC').format(options.format as string)
+  } catch (err) {
+    return ''
+  }
 }
