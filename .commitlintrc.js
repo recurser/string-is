cff772c3942b8798bc24b25aa3d69ed03c0dee8b
Dave Perrett <dave@recurser.com>
2022-01-04 14:02:50 +0900

    build: allow longer commit messages so that dependabot PRs pass

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Dependabot lines can get quite long unfortunately:
+    'body-max-line-length': () => [2, 'always', 150],
+  },
+}
