90d29565a9e69ad401e5d2dcdc39db7c6e383e19
Dave Perrett <dave@recurser.com>
2022-01-06 00:31:22 +0900

    feat: add a Markdown-to-HTML converter

---
+++ src/lib/converters/MarkdownToHtmlConverter.ts
@@ -0,0 +1,18 @@
+import { Converter } from 'showdown'
+
+import { output as htmlOutput } from '@lib/outputs/HtmlOutput'
+import { ConverterOptions } from '@lib/types'
+
+export const id = 'markdownToHtml'
+
+export const outputId = 'html'
+
+export const operation = (
+  input: string,
+  options: ConverterOptions = {},
+): string => {
+  const converter = new Converter({ noHeaderId: true })
+  const html = converter.makeHtml(input)
+
+  return htmlOutput(html, options)
+}
