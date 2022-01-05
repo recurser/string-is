cc246d2986a64c30bc2eaf47121165dbfe0d7b5b
Dave Perrett <dave@recurser.com>
2022-01-05 20:04:00 +0900

    perf: remove inefficient multi-line CSS regexes that were causing freezes in long strings

--- src/lib/identities/ScssIdentity.ts
+++ src/lib/identities/ScssIdentity.ts
@@ -1,4 +1,6 @@
 import { isEmpty } from 'lodash'
+import parserPostcss from 'prettier/parser-postcss'
+import { format } from 'prettier/standalone'
 
 import { Converter, ScssFormatter } from '@lib/converters'
 
@@ -9,25 +11,10 @@ export const confidence = (input: string) => {
     return 0
   }
 
-  if (
-    // We must have at least a word with opening and closing brackets, with a semicolon inside.
-    // ([\.#]?[^{\s]+) : Anything that's not a space or a '{'
-    // [\s]*           : Optional spaces before the '{ ... }'
-    // {               : Open the brackets.
-    // [^\:]+:         : Anything that's not a ':', followed by a ':'.
-    // [^;]+;          : Anything that's not a ';', followed by a ';'.
-    // (.|\s)*         : Any number of characters / spaces / line breaks.
-    // [{}](.|\s)*     : An extra bracket nested inside somewhere. <-- THIS IS THE ONLY DIFFERENCE FROM THE CSS REGEX.
-    // }               : The closing bracket.
-    !/([\.#]?[^{\s]+)[\s]*{[^\:]+:[^;]+;(.|\s)*[{}](.|\s)*}/gm.test(
-      input.trim(),
-    )
-  ) {
-    return 0
-  }
+  // Prettier will throw an exception if this fails.
+  format(input, { parser: 'scss', plugins: [parserPostcss] })
 
-  // Let the LessFormatter take precedence, if it gets a match.
-  return 90
+  return 100
 }
 
 export const converters = [ScssFormatter] as Converter[]
