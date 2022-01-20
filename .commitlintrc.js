ca6fc5c75a37ff86879aaaa6c4ca164ba6e9eec2
Dave Perrett <dave@recurser.com>
2022-01-20 19:30:26 +0900

    build(deps-dev): allow longer commit messages to keep commitlint + dependabot happy

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     // Dependabot lines can get quite long unfortunately:
-    'body-max-line-length': () => [2, 'always', 150],
+    'body-max-line-length': () => [2, 'always', 200],
   },
 }
