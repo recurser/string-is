5fffa1f859490b34efbfa100aaac3a22227d6e50
Dave Perrett <dave@recurser.com>
2022-01-25 10:42:03 +0900

    test: render translations in tests

--- src/components/pages/__tests__/Convert.test.tsx
+++ src/components/pages/__tests__/Convert.test.tsx
@@ -3,19 +3,28 @@
  */
 
 import { render, screen } from '@testing-library/react'
+import I18nProvider from 'next-translate/I18nProvider'
 import React from 'react'
 import { act } from 'react-dom/test-utils'
 
 import { Convert } from '@pages/Convert'
+import translations from '@locales/en/domain-convert-inputForm'
 
 describe('Convert', () => {
   it('renders a heading', async () => {
     await act(async () => {
-      render(<Convert />)
+      render(
+        <I18nProvider
+          lang={'en'}
+          namespaces={{ 'domain-convert-inputForm': translations }}
+        >
+          <Convert />
+        </I18nProvider>,
+      )
     })
 
     const heading = screen.getByRole('label', {
-      name: /domain-convert-InputForm:label/i,
+      name: translations.label,
     })
 
     expect(heading).toBeInTheDocument()
--- src/components/pages/__tests__/Terms.test.tsx
+++ src/components/pages/__tests__/Terms.test.tsx
@@ -3,16 +3,22 @@
  */
 
 import { render, screen } from '@testing-library/react'
+import I18nProvider from 'next-translate/I18nProvider'
 import React from 'react'
 
 import { Terms } from '@pages/Terms'
+import translations from '@locales/en/pages-terms'
 
 describe('Terms', () => {
   it('renders a heading', () => {
-    render(<Terms />)
+    render(
+      <I18nProvider lang={'en'} namespaces={{ 'pages-terms': translations }}>
+        <Terms />
+      </I18nProvider>,
+    )
 
     const heading = screen.getByRole('heading', {
-      name: /pages-terms:page_heading/i,
+      name: translations.page_heading,
     })
 
     expect(heading).toBeInTheDocument()
