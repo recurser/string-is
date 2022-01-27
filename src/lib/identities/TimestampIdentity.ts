ce04c730498e6157ec9e7c200aa59ccf1a1c6ba9
Dave Perrett <dave@recurser.com>
2022-01-27 11:08:35 +0900

    feat: add keywords to trigger the current timestamp as input (now / time / timestamp)

--- src/lib/identities/TimestampIdentity.ts
+++ src/lib/identities/TimestampIdentity.ts
@@ -1,19 +1,10 @@
-import { isEmpty } from 'lodash'
-
 import { Converter, TimestampConverter } from '@lib/converters'
+import { input as timestampInput } from '@lib/inputs/TimestampInput'
 
 export const id = 'timestamp'
 
-// See https://stackoverflow.com/a/8571649
 export const confidence = (input: string) => {
-  if (isEmpty(input)) {
-    return 0
-  }
-
-  if (
-    // We assume the timestamp will start with '1'.
-    !/^1(\d{9}|\d{12})$/.test(input.trim())
-  ) {
+  if (timestampInput(input) === undefined) {
     return 0
   }
 
