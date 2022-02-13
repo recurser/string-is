c8ca9d70765f037a17e04bce428bb8d7f5f19b63
Dave Perrett <dave@recurser.com>
2022-02-13 22:42:37 +0900

    test: add services/ tests

---
+++ src/services/__tests__/Converter.test.ts
@@ -0,0 +1,38 @@
+import {
+  Base64Encoder,
+  HtmlFormatter,
+  Md5Encoder,
+  RipemdEncoder,
+  ShaEncoder,
+  UpperCaseConverter,
+  UrlEncoder,
+} from '@lib/converters'
+
+import { converterCandidates } from '@services/Converter'
+
+describe('services', () => {
+  describe('Converter', () => {
+    describe('converterCandidates', () => {
+      it('returns multiple candidates', async () => {
+        const input = 'plain text!'
+        const expected = [
+          Base64Encoder,
+          Md5Encoder,
+          RipemdEncoder,
+          ShaEncoder,
+          UpperCaseConverter,
+          UrlEncoder,
+        ]
+        const result = await converterCandidates(input)
+        expect(result).toEqual(expected)
+      })
+
+      it('returns single candidates', async () => {
+        const input = '<b>html</b>'
+        const expected = [HtmlFormatter]
+        const result = await converterCandidates(input)
+        expect(result).toEqual(expected)
+      })
+    })
+  })
+})
