fd96b6a4fa12d47f6ba2d76947f2f8b2702f679a
Dave Perrett <dave@recurser.com>
2022-03-01 00:55:55 +0900

    feat: add a JSON-to-JavaScript converter

---
+++ src/lib/converters/JsonToJavaScriptConverter.ts
@@ -0,0 +1,34 @@
+import { ConverterOptions } from '@lib/types'
+import { output as javaScriptOutput } from '@lib/outputs/JavaScriptOutput'
+import { input as jsonInput } from '@lib/inputs/JsonInput'
+
+/**
+ * A string which uniquely identifies this operation.
+ */
+export const id = 'jsonToJavaScript'
+
+/**
+ * A string which uniquely identifies the output component used by
+ * this converter.
+ */
+export const outputId = 'javaScript'
+
+/**
+ * An operation that formats the given JSON input string as JavaScript.
+ *
+ * @param input   - the string to convert.
+ * @param options - options that control the conversion process.
+ *
+ * @returns the converted string.
+ */
+export const operation = (
+  input: string,
+  options: ConverterOptions = {},
+): string => {
+  const obj = jsonInput(input)
+  if (!obj) {
+    return ''
+  }
+
+  return javaScriptOutput(`const data = ${JSON.stringify(obj)}`, options)
+}
