bd6e869a496c0146a93f01725f721e8d33d1510d
Dave Perrett <dave@recurser.com>
2022-03-22 22:51:10 +0900

    fix: avoid layout issues in the header title on mobile

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,6 +14,7 @@ import useTranslation from 'next-translate/useTranslation'
 import { ROUTE_ABOUT, ROUTE_CONVERT } from '@services/Routes'
 import { Link } from '@components/navigation'
 import Logo from '@images/logo.svg'
+import { useBreakpoints } from '@services/Responsive'
 
 interface Props {
   pageHeading?: string | ReactElement
@@ -24,47 +25,62 @@ interface Props {
  */
 export const Header = ({ pageHeading }: Props): ReactElement => {
   const { t } = useTranslation('layout-header')
+  const { isMobile } = useBreakpoints()
   const { pathname } = useRouter()
 
   return (
-    <Pane
-      alignItems="center"
-      display="flex"
-      flexDirection="row"
-      justifyContent="space-between"
-      marginBottom={majorScale(2)}
-    >
-      <Pane alignItems="center" display="flex">
-        <Link href={ROUTE_CONVERT} lineHeight={0}>
-          <Image
-            alt="string-is logo"
-            display="inline-flex"
-            height={majorScale(5)}
-            src={`${Logo.src}`}
-            width={majorScale(5)}
-          />
-        </Link>
-        {pageHeading ? (
-          <Heading
-            display="inline-flex"
-            height="100%"
-            is="h1"
-            marginLeft={majorScale(2)}
-          >
-            {pageHeading}
-          </Heading>
-        ) : null}
-      </Pane>
-      <Pane>
-        <Tablist display="flex" gap={minorScale(1)}>
-          <Link href={ROUTE_ABOUT}>
-            <Tab isSelected={pathname === ROUTE_ABOUT}>{t('menu_about')}</Tab>
+    <Pane flexDirection="column">
+      <Pane
+        alignItems="center"
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        marginBottom={majorScale(2)}
+      >
+        <Pane alignItems="center" display="flex">
+          <Link href={ROUTE_CONVERT} lineHeight={0}>
+            <Image
+              alt="string-is logo"
+              display="inline-flex"
+              height={majorScale(5)}
+              src={`${Logo.src}`}
+              width={majorScale(5)}
+            />
           </Link>
-          <Link href="https://github.com/recurser/string-is" target="_blank">
-            <Tab isSelected={false}>{t('menu_github')}</Tab>
-          </Link>
-        </Tablist>
+          {pageHeading && !isMobile ? ( // Non-mobile header, next to the logo.
+            <Heading
+              display="inline-flex"
+              height="100%"
+              is="h1"
+              marginLeft={majorScale(2)}
+            >
+              {pageHeading}
+            </Heading>
+          ) : null}
+        </Pane>
+
+        <Pane>
+          <Tablist display="flex" gap={minorScale(1)}>
+            <Link href={ROUTE_ABOUT}>
+              <Tab isSelected={pathname === ROUTE_ABOUT}>{t('menu_about')}</Tab>
+            </Link>
+            <Link href="https://github.com/recurser/string-is" target="_blank">
+              <Tab isSelected={false}>{t('menu_github')}</Tab>
+            </Link>
+          </Tablist>
+        </Pane>
       </Pane>
+
+      {pageHeading && isMobile ? ( // Mobile header, below the logo an menu.
+        <Heading
+          is="h1"
+          marginBottom={majorScale(2)}
+          marginTop={majorScale(2)}
+          textAlign="center"
+        >
+          {pageHeading}
+        </Heading>
+      ) : null}
     </Pane>
   )
 }
