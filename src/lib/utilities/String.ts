4ae02de3fe7de973acee48b7ce62fcee62fd82d3
Dave Perrett <dave@recurser.com>
2022-03-23 08:24:16 +0900

    feat: change the URL when a converter is selected

--- src/lib/utilities/String.ts
+++ src/lib/utilities/String.ts
@@ -1,6 +1,37 @@
-import { fromPairs, isArray, isObject, map } from 'lodash'
+import { camelCase, fromPairs, isArray, isObject, kebabCase, map } from 'lodash'
 
 import { Obj } from '@lib/types'
+
+/**
+ * Converts a hyphenated converter slug to a camel-cased ID identifying
+ * the converter module.
+ *
+ * @param converterSlug - The hyphenated slug to camel-case.
+ *
+ * @returns the camel-cased slug.
+ */
+export const camelCaseConverterSlug = (converterSlug: string): string => {
+  // Hyphenated 'JavaScript' is more readable as 'javascript' in URLs.
+  const adjusted = converterSlug.replace('javascript', 'java-script')
+  return camelCase(adjusted)
+}
+
+/**
+ * Converts a camel-cased converter ID to a hyphenated slug suitable
+ * for use in the converter URL.
+ *
+ * @param converterId - The camel-cased ID to hyphenate.
+ *
+ * @returns the hyphenated slug.
+ */
+export const hyphenateConverterId = (converterId: string): string => {
+  // Hyphenated 'JavaScript' is more readable as 'javascript' in URLs.
+  let adjusted = converterId.replace('avaScript', 'avascript')
+  adjusted = kebabCase(adjusted)
+  // Remove the hyphen from numeric values like 'base-64' and 'md-5'.
+  adjusted = adjusted.replace(/\-(\d)/, '$1')
+  return adjusted
+}
 
 /**
  * Sorts the given object by it's keys, recursively.
