f9efa58bce3fb45fa4dfd6e38df27bef60fe43db
Dave Perrett <dave@recurser.com>
2022-04-13 08:22:06 +0900

    feat: add a TOML to JSON converter

---
+++ src/lib/converters/TomlToJsonConverter.ts
@@ -0,0 +1,34 @@
+import type { ConverterOptions, Obj } from '@lib/types'
+import { output as jsonOutput } from '@lib/outputs/JsonOutput'
+import { input as tomlInput } from '@lib/inputs/TomlInput'
+
+/**
+ * A string which uniquely identifies this operation.
+ */
+export const id = 'tomlToJsonConverter'
+
+/**
+ * A string which uniquely identifies the output component used by
+ * this converter.
+ */
+export const outputId = 'json'
+
+/**
+ * An operation that formats the given TOML input string as JSON.
+ *
+ * @param input   - the string to convert.
+ * @param options - options that control the conversion process.
+ *
+ * @returns the converted string.
+ */
+export const operation = (
+  input: string,
+  options: ConverterOptions = {},
+): string => {
+  const obj = tomlInput(input)
+  if (!obj) {
+    return ''
+  }
+
+  return jsonOutput(obj as Obj, options)
+}
