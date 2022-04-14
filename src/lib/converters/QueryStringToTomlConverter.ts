9c15a3e5c8a3f7ce9903b28f1f1d7d7e86350e38
Dave Perrett <dave@recurser.com>
2022-04-14 21:37:15 +0900

    feat: add a query string to TOML converter

--- src/lib/converters/QueryStringToTomlConverter.ts
+++ src/lib/converters/QueryStringToTomlConverter.ts
@@ -1,20 +1,20 @@
 import type { ConverterOptions, Obj } from '@lib/types'
 import { input as queryStringInput } from '@lib/inputs/QueryStringInput'
-import { output as yamlOutput } from '@lib/outputs/YamlOutput'
+import { output as tomlOutput } from '@lib/outputs/TomlOutput'
 
 /**
  * A string which uniquely identifies this operation.
  */
-export const id = 'queryStringToYamlConverter'
+export const id = 'queryStringToTomlConverter'
 
 /**
  * A string which uniquely identifies the output component used by
  * this converter.
  */
-export const outputId = 'yaml'
+export const outputId = 'toml'
 
 /**
- * An operation that converts the given browser query string to YAML.
+ * An operation that converts the given browser query string to TOML.
  *
  * @param input   - the string to convert.
  * @param options - options that control the conversion process.
@@ -30,5 +30,5 @@ export const operation = (
     return ''
   }
 
-  return yamlOutput(obj as Obj, options)
+  return tomlOutput(obj as Obj, options)
 }
