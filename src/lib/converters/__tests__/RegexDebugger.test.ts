a662287ffa2cc770f501ee1a8079933b0681fe84
Dave Perrett <dave@recurser.com>
2022-04-19 23:49:16 +0900

    fix: resolve potential race conditions in the regex debugger tests

--- src/lib/converters/__tests__/RegexDebugger.test.ts
+++ src/lib/converters/__tests__/RegexDebugger.test.ts
@@ -1,27 +1,28 @@
-import { assertOutput, expectError } from './_helpers'
 import { fireEvent, screen } from '@testing-library/react'
+
+import { assertOutput, expectError } from './_helpers'
 import { RegexDebugger } from '@lib/converters'
 
 describe('converters', () => {
   describe('RegexDebugger', () => {
     it('displays a single match', async () => {
-      await assertOutput(RegexDebugger, '/find this/', () => {
-        const inputElement = screen.getByTestId('regex-input')
+      await assertOutput(RegexDebugger, '/find this/', async () => {
+        const inputElement = await screen.getByTestId('regex-input')
         fireEvent.change(inputElement, {
           target: { value: 'can you find this?' },
         })
 
-        const matchOutput = screen.getByTestId('regex-match-output')
+        const matchOutput = await screen.getByTestId('regex-match-output')
         expect(matchOutput.value).toEqual('find this')
 
-        const alert = screen.getByTestId('output-success')
+        const alert = await screen.getByTestId('output-success')
         expect(alert).toHaveTextContent('alert_single_match')
       })
     })
 
     it('displays multiple matches', async () => {
-      await assertOutput(RegexDebugger, '/find this/g', () => {
-        const inputElement = screen.getByTestId('regex-input')
+      await assertOutput(RegexDebugger, '/find this/g', async () => {
+        const inputElement = await screen.getByTestId('regex-input')
         fireEvent.change(inputElement, {
           target: { value: 'can you find this?\ncan you find this?' },
         })
@@ -41,19 +42,19 @@ describe('converters', () => {
     })
 
     it('handles captured groups', async () => {
-      await assertOutput(RegexDebugger, '/find (this)/', () => {
-        const inputElement = screen.getByTestId('regex-input')
+      await assertOutput(RegexDebugger, '/find (this)/', async () => {
+        const inputElement = await screen.getByTestId('regex-input')
         fireEvent.change(inputElement, {
           target: { value: 'can you find this?' },
         })
 
-        const matchOutput = screen.getByTestId('regex-match-output')
+        const matchOutput = await screen.getByTestId('regex-match-output')
         expect(matchOutput.value).toEqual('find this')
 
-        const groupOutput = screen.getByTestId('regex-group-output')
+        const groupOutput = await screen.getByTestId('regex-group-output')
         expect(groupOutput.value).toEqual('this')
 
-        const alert = screen.getByTestId('output-success')
+        const alert = await screen.getByTestId('output-success')
         expect(alert).toHaveTextContent('alert_single_match')
       })
     })
