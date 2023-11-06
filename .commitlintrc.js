a1f8b4a6aece6e3fd3d8115a8a201bb026323d1a
Dave Perrett <dave@recurser.com>
2023-11-06 13:31:17 +0900

    fix(build): allow longer commit footers

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     // Dependabot lines can get quite long unfortunately:
     'body-max-line-length': () => [2, 'always', 250],
+    'footer-max-line-length': () => [2, 'always', 250],
     'type-enum': [
       2,
       'always',
